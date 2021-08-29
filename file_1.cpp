4616591fe6d4491411df89d0847fb365439ba41d
Svetlana <slavak555@rambler.ru>
2021-08-29 16:45:03 +0400

    file_1 izm file_2 new

---
+++ file_1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    cout<<"file_1()1"<< end;
+}
---
+++ file_2.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    cout<<"file_2(1)"5<< end;
+}
